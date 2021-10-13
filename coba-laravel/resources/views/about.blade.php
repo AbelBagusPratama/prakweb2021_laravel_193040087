f7860e47cb98daf3bf4010ec7b2cd6a56ee0dfc6
AbelBagusPratama <58681377+AbelBagusPratama@users.noreply.github.com>
2021-10-13 21:08:28 +0700

    Menyelesaikan Struktur Folder,Routing Dan View Video 3 Pertemuan 5

---
+++ coba-laravel/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css"
+    <title>WPU Blog | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1> 
+    <h3>Abel Bagus Pratama</h3>
+    <p>abelbagusp@gmail.com</p>
+    <img src="img/abel.jpg" alt="Abel" width="200"
+</body>
+</html>
