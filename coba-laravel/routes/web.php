15eb3e2bd5e5e3cf8a3f3b73fe480f9d50e67248
AbelBagusPratama <58681377+AbelBagusPratama@users.noreply.github.com>
2021-10-20 20:16:30 +0700

    Menyelesaikan Model, Collection dan Controler

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home" 
     ]);
 });
 
@@ -23,60 +26,15 @@ Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         "name" => "Abel Bagus Pratama",
-        "email"=> "abelbagusp@gmail.com",
+        "email" => "abelbagusp@gmail.com",
         "image" => "abel.jpg"
     ]);
 });
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Abel Bagus Pratama",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quia praesentium facere hic, ad necessitatibus magnam recusandae cupiditate, amet quod rerum. Quibusdam ipsam vero fuga! Eaque laborum impedit soluta itaque blanditiis repellat esse labore minus nam eos, assumenda optio deleniti harum at atque iusto ducimus neque. Culpa non laboriosam, consequuntur ducimus aspernatur cumque pariatur repudiandae impedit natus exercitationem, sit a temporibus iste. Dolorum est odit, ratione fugit sint, laboriosam vitae corrupti quidem voluptates aspernatur iste ducimus voluptatibus molestias dolor sunt."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tono Jon",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur molestiae, eaque id unde ratione distinctio dolores nihil maxime tenetur aut labore fugit, quasi molestias quo reprehenderit cum sit. Expedita, incidunt provident. Quod, dolore odit illum autem dicta saepe. Ratione veritatis praesentium optio quaerat non ut similique reiciendis iste hic qui eveniet quas sit porro est, architecto in laudantium doloremque quasi? Tenetur fugiat, ipsam iste maxime reiciendis nam nobis voluptatum officiis delectus inventore eos sapiente porro corporis consequatur, earum fuga placeat eveniet mollitia laboriosam! Itaque, veritatis libero quod quisquam tenetur atque veniam est ipsam quae quaerat? Accusantium enim voluptas deserunt consequuntur!"
-        ],
-    ];
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Abel Bagus Pratama",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quia praesentium facere hic, ad necessitatibus magnam recusandae cupiditate, amet quod rerum. Quibusdam ipsam vero fuga! Eaque laborum impedit soluta itaque blanditiis repellat esse labore minus nam eos, assumenda optio deleniti harum at atque iusto ducimus neque. Culpa non laboriosam, consequuntur ducimus aspernatur cumque pariatur repudiandae impedit natus exercitationem, sit a temporibus iste. Dolorum est odit, ratione fugit sint, laboriosam vitae corrupti quidem voluptates aspernatur iste ducimus voluptatibus molestias dolor sunt."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tono Jon",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur molestiae, eaque id unde ratione distinctio dolores nihil maxime tenetur aut labore fugit, quasi molestias quo reprehenderit cum sit. Expedita, incidunt provident. Quod, dolore odit illum autem dicta saepe. Ratione veritatis praesentium optio quaerat non ut similique reiciendis iste hic qui eveniet quas sit porro est, architecto in laudantium doloremque quasi? Tenetur fugiat, ipsam iste maxime reiciendis nam nobis voluptatum officiis delectus inventore eos sapiente porro corporis consequatur, earum fuga placeat eveniet mollitia laboriosam! Itaque, veritatis libero quod quisquam tenetur atque veniam est ipsam quae quaerat? Accusantium enim voluptas deserunt consequuntur!"
-        ],
-    ];
+//halaman single post
 
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post',[
-        "title" => "Single Pos",
-        "post" => $new_post 
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
